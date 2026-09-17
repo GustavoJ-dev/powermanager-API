dfe264a905853ab6b80caedf356307d4220ee66e
Gustavo De Jesus Silva <gustavo.jesus.silva.dev@gmail.com>
2026-09-17 17:49:02 -0300

    chore: Configuração inicial do projeto

---
+++ src/main/java/com/powermanager/powermanagerAPI/PowermanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.powermanager.powermanagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowermanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowermanagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powermanager/powermanagerAPI/PowermanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powermanager.powermanagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowermanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
